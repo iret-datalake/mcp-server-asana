7580bdee7a8f6189ef8a2e977bda2544517edbda
hesai <sai@iret.co.jp>
2025-07-25 12:26:27 +0900

    feat: add projects name to default fields in searchTasks method

--- src/asana-client-wrapper.ts
+++ src/asana-client-wrapper.ts
@@ -90,7 +90,7 @@ export class AsanaClientWrapper {
     if (is_blocking !== undefined) searchParams.is_blocking = is_blocking;
     if (sort_by) searchParams.sort_by = sort_by;
     if (sort_ascending !== undefined) searchParams.sort_ascending = sort_ascending;
-    const defaultFields = 'gid,name,completed,created_at,modified_at,resource_subtype,custom_fields,assignee.name,notes,permalink_url';
+    const defaultFields = 'gid,name,completed,created_at,modified_at,resource_subtype,custom_fields,assignee.name,notes,permalink_url,projects.name';
     if (opt_fields !== undefined) {
       searchParams.opt_fields = opt_fields + ',' + defaultFields;
     } else {
